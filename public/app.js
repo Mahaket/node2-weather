745bc30ba00a4a1280284ea23715a03668afde8e
PPketan <patilketan54321@gmail.com>
2022-05-13 12:22:17 +0530

    spelling changed

--- public/app.js
+++ public/app.js
@@ -1,13 +1,34 @@
 const url = 'http://api.weatherstack.com/current?access_key=850cfda6651dc44a72e691481bd16488&query=+ encodeURIComponent(latitude, longitude) +&units=m'
 
-fetch('urhttp://localhost:3000/weather?address=Mumbai').then((response) => {
-    response.json().then((data) => {
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+          e.preventDefault()
+
+          const location = search.value
+
+          messageOne.textContent = 'Loading weather'
+
+          fetch('urhttp://localhost:3000/weather?address=' + location).then((response) => {
+          response.json().then((data) => {
         if(data.error){
-            console.log('Try another search')
+
+            messageTwo.textContent = data.error
+
         } else {
+
             console.log(data.temperature)
             console.log(data.location)
         }
     })
 })
+
+
+} )
+
 
